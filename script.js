b055c71fb04e645a3584605eecab8456c67fce37
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-06-01 03:04:14 -0300

    Update Json CSS

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 import data from "./JSON/data.json" with { type: "json"};
 
-// const parsed = JSON.parse(data);
-console.log(data)
+let arr = ['Reaction', 'Memory', 'Verbal', 'Visual']
+
+arr.forEach(category => {
+    data.forEach(item => {
+        if(category === item.category){
+            const categoryElement = document.getElementById(item.category);
+
+            const newImage = document.createElement('img');
+            newImage.src = item.icon;
+            newImage.className = 'image';
+            newImage.alt = 'Icon for category';
+            categoryElement.appendChild(newImage);
+
+            const newCategory = document.createElement('h3');
+            newCategory.innerHTML = item.category;
+            newCategory.className = 'category';
+            categoryElement.appendChild(newCategory);
+
+            const newScore = document.createElement('p');
+            newScore.innerHTML = item.score;
+            newScore.className = 'point';
+            categoryElement.appendChild(newScore);
+
+            const total = document.createElement('p');
+            total.innerHTML = '/ 100';
+            total.className = 'total';
+            categoryElement.appendChild(total);
+
+        }
+    });
+});
